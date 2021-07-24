8a60e5c52826972cff198b2caa4aa55bc930fe7c
Michael jentsch <jentsch.michael@gmail.com>
2021-07-24 15:32:19 +0200

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/jentsch/app_icon_changer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/jentsch/app_icon_changer/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.w3logistics.app_icon_changer", appContext.getPackageName());
+        assertEquals("com.jentsch.app_icon_changer", appContext.getPackageName());
     }
 }
